a3d084a4272bf6a95c39e4d1958b0d865f48cf19
Aliaksandr <djvalentinoff@gmail.com>
2022-06-09 16:27:18 +0300

    feat: add section hero

---
+++ js/main.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  
+// slider-hero start
+
+const swiper = new Swiper('.hero__swiper', {
+  allowTouchMove: false,
+  loop: true,
+  effect: 'fade',
+  speed: 8000,
+  autoplay: {
+    delay: 5000
+  }
+});
+
+// slider-hero finish
+});
